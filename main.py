15a57b92996e3854c32709cd36fc36f3ec76ae1a
USER USER <yoto8644@gmail.com>
2025-04-13 11:33:08 +0500

    Add CORS

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 import os
 import uvicorn
 
 from api.generate_text import generate_text_router
 from api.generate_image import generate_image_router
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:63342",
+    "http://127.0.0.1",
+    "http://127.0.0.1:8080",
+    "http://127.0.0.1:63342",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешаем все методы
+    allow_headers=["*"],  # Разрешаем все заголовки
+)
 
 app.include_router(generate_text_router.router, tags=["Text"])
 app.include_router(generate_image_router.router, tags=["Image"])
